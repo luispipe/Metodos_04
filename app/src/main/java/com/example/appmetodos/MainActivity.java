1f7e10a6c7c874484d5d39a097d5e4e8283a3821
luispipe <lflondonor@unal.edu.co>
2023-09-27 09:25:20 -0300

    final Commit

--- app/src/main/java/com/example/appmetodos/MainActivity.java
+++ app/src/main/java/com/example/appmetodos/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.appmetodos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
     //Una Clase (class) puede tener n cantidad de funciones o metodos
 
-    TextView tv_imc;
+    TextView tv_imc,tv_classification;
     EditText name, weight, height;
     Button calculate;
     @Override
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_imc= findViewById(R.id.tvIMC);
+        tv_classification=findViewById(R.id.tvClassification);
         name=findViewById(R.id.etName);
         weight=findViewById(R.id.etWeight);
         height=findViewById(R.id.etHeight);
@@ -43,12 +48,51 @@ public class MainActivity extends AppCompatActivity {
                     double altura= Double.parseDouble(heightTexto);
                     double imcResult= calcularIMC(peso,altura);
                     message(imcResult,nameText);
+                    claficacionIMC(imcResult);
                 }
             }
         });
 
 
     }
+    /*Definición de variables y constantes
+    *Variables:
+    1. tipo de dato ->  Nativos:String,char,boolean,byte,short,int,lon,float,double
+                    ->  otros tipos: Button,Intent,TextView,EditText
+
+    2. Nombre ->  Solo carácteres alfanúmericos
+              ->  Se permite el uso del _
+              ->  Un nombre no puede iniciar con números
+              ->  No se pueden definir nombres de variables iguales
+                  a palabras reservadas del sistemas. Ej: for,if,public,protected,private,final,while,else,etc.
+              -> No puedo tener dos variables con el mismo nombre
+              -> Las variables comienzan por minuscula
+    3. Asignar un valor -> el valor debe ser del mismo tipo de dato de la variable
+                        -> Esto no aplica para tipo de dato double que recibe cualquier tipo de dato de enteros
+
+    Ejemplos: String nombre="Luis";
+              int edad= 30;
+              double estatura= 170;
+              boolean esMayorEdad= true;
+
+    Constantes:
+    1. Agregar la etiqueta final
+    2. tipo de dato ->  Nativos:String,char,boolean,byte,short,int,lon,float,double
+                    ->  otros tipos: Button,Intent,TextView,EditText
+    2. Nombre ->  Solo carácteres alfanúmericos
+              ->  Se permite el uso del _
+              ->  Un nombre no puede iniciar con números
+              ->  No se pueden definir nombres de constantes iguales
+                  a palabras reservadas del sistemas. Ej: for,if,public,protected,private,final,while,else,etc.
+              -> No puedo tener dos constantes con el mismo nombre
+              -> El nombre de las constantes va por completo en mayuscula
+
+    3. Asignar un valor -> el valor debe ser del mismo tipo de dato de la variable
+                        -> Esto no aplica para tipo de dato double que recibe cualquier tipo de dato de enteros
+
+    Ejemplos: final String UNIVERSIDAD="Universidad Nacional de Colombia";
+              final int NIT= 87463214;
+      */
 
     //Declaración de metodos
     //Primero va el nivel de acceso(public,protected,privado,default)
@@ -102,11 +146,34 @@ public class MainActivity extends AppCompatActivity {
     protected void message(double imc, String nombre){
         System.out.println("El usuario: "+nombre+ ", tiene un IMC de "+imc);
         // "El usuario: Luis, tiene un IMC de 40"
-        tv_imc.setText("El usuario: "+nombre+ ", tiene un IMC de "+imc);
+        //Redondear o limitar los decimales del imc
+        double imc2= Math.round(imc*100d)/100d;
+        tv_imc.setText("El usuario: "+nombre+ ", tiene un IMC de "+imc2);
         //Dentro de String hay una función de concatenar o combinar (+)
         // 55+""="55",  25.5 +"Hola"= "25.5Hola"
     }
 
+    //Metodo que permita definir la clasificación del IMC de un usuario
+
+    public void claficacionIMC(double imc){
+        if(imc<18.5){
+            tv_classification.setText("Bajo Peso");
+            tv_classification.setTextColor(Color.YELLOW);
+        } else if (imc>=18.5 && imc<24.99) {
+            tv_classification.setText("Peso Normal");
+            tv_classification.setGravity(Gravity.CENTER);
+            tv_classification.setTextColor(Color.GREEN);
+        } else if (imc>=25 && imc<30) {
+            tv_classification.setText("Sobrepeso");
+            tv_classification.setTextColor(Color.MAGENTA);
+        } else if (imc>=30) {
+            tv_classification.setText("Obesidad");
+            tv_classification.setTextColor(Color.RED);
+        } else {
+            tv_classification.setText("No se pudo calcular el IMC");
+            tv_classification.setTextColor(Color.CYAN);
+        }
+    }
 
 
 
